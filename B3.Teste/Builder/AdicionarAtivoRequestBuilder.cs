a813fb24802ab01a953ddc6952107b4a2f9d62c1
esfreitas <eltons.freitas01@gmail.com>
2021-04-11 12:44:59 -0300

    Teste feito durante a aula implementado, mas ainda está pegando um erro, falta confereir e fazer os demais testes de useCase

---
+++ B3.Teste/Builder/AdicionarAtivoRequestBuilder.cs
@@ -0,0 +1,35 @@
+﻿using B3.DTO.Ativo.AdicionarAtivo;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B3.Teste.Builder
+{
+    public class AdicionarAtivoRequestBuilder
+    {
+        private readonly Faker _faker = new Faker("pt_BR");
+        private readonly AdicionarAtivoRequest _adicionarAtivo;
+
+        public AdicionarAtivoRequestBuilder()
+        {
+            _adicionarAtivo = new AdicionarAtivoRequest();
+            _adicionarAtivo.empresa = _faker.Random.String(40);
+            _adicionarAtivo.codigoB3 = _faker.Random.String(40);
+        }
+
+        public AdicionarAtivoRequestBuilder withEmpresaLength(int tamanho)
+        {
+            _adicionarAtivo.empresa = _faker.Random.String(tamanho);
+            return this;
+        }
+
+
+        public AdicionarAtivoRequest Build()
+        {
+            return _adicionarAtivo;
+        }
+    }
+}
---
+++ B3.Teste/UseCase/AdicionarAtivoUseCaseTest.cs
@@ -0,0 +1,89 @@
+﻿using B3.Bordas.Adapter;
+using B3.Bordas.Repositorio;
+using B3.DTO.Ativo.AdicionarAtivo;
+using B3.Entities;
+using B3.Teste.Builder;
+using B3.UseCase;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace B3.Teste.UseCase
+{
+    public class AdicionarAtivoUseCaseTest
+    {
+        private readonly Mock<IRepositorioAtivos> _repositorioAtivos;
+        private readonly Mock<IAdicionarAtivoAdapter> _adicionarAtivoAdapter;
+        private readonly AdicionarAtivoUseCase _usecase;
+
+        public AdicionarAtivoUseCaseTest()
+        {
+            _repositorioAtivos = new Mock<IRepositorioAtivos>();
+            _adicionarAtivoAdapter = new Mock<IAdicionarAtivoAdapter>();
+            _usecase = new AdicionarAtivoUseCase(
+                _repositorioAtivos.Object, 
+                _adicionarAtivoAdapter.Object
+                );
+        }
+
+        [Fact]
+        public void Ativo_AdicionarAtivo_QuandoRetornarSucesso()
+        {
+            //Arrange
+            var request = new AdicionarAtivoRequestBuilder().Build();
+            var response = new AdicionarAtivoResponse();
+            var ativo = new Ativo();
+            var ativoId = 1;
+            _repositorioAtivos.Setup(repositorio => repositorio.Add(ativo)).Returns(ativoId);
+            _adicionarAtivoAdapter.Setup(adapter => adapter.converterRequestParaAtivo(request)).Returns(ativo);
+
+            //Act
+            var result = _usecase.Executar(request);
+
+            //Assert
+            response.Should().BeEquivalentTo(result);
+
+        }
+
+        [Fact]
+        public void Ativo_AdicionarAtivo_QuantoEmpresaMenorVinte()
+        {
+            //Arrange
+            var request = new AdicionarAtivoRequestBuilder().withEmpresaLength(10).Build();
+            var respose = new AdicionarAtivoResponse();
+         
+            respose.msg = "Erro ao adicionar o Ativo";
+
+            //Act
+            var result = _usecase.Executar(request);
+
+            //Assert
+            respose.Should().BeEquivalentTo(result);
+        }
+
+        [Fact]
+        public void Ativo_AdicionarAtivo_QuantoRepositorioExcecao()
+        {
+            //Arrange
+            var request = new AdicionarAtivoRequestBuilder().Build();
+            var respose = new AdicionarAtivoResponse();
+            var ativo = new Ativo();
+            ativo.id = 1;
+            respose.msg = "Erro ao adicionar o produto";
+            _adicionarAtivoAdapter.Setup(adapter => adapter.converterRequestParaAtivo(request)).Returns(ativo);
+            _repositorioAtivos.Setup(repositorio => repositorio.Add(ativo)).Throws(new Exception());
+
+            //Act
+            var result = _usecase.Executar(request);
+
+            //Assert
+            respose.Should().BeEquivalentTo(result);
+        }
+
+    }
+}
